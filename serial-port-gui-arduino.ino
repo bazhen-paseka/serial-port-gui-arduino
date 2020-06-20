85d29cb8dd1329843b5c29e1dc2a790ffa4ce1c2
Бажен Левковець <bazhen.paseka@gmail.com>
2020-06-20 19:41:58 +0300

    initial

---
+++ serial-port-gui-arduino.ino
@@ -0,0 +1,25 @@
+void setup() {
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+int val = 12;
+void loop() {
+ if (Serial.available() > 0) {
+    char a = Serial.read();
+    val++;
+    if (a == 'b') {
+      digitalWrite(13, LOW);
+      Serial.print("    off");
+    } else if (a == 'r') {
+      digitalWrite(13, HIGH);
+      Serial.print("On");
+    } else if (a == 'p') {
+      //int val = analogRead(0);
+      Serial.print(val);
+    } else {
+      Serial.print("Error!");
+    }
+  }
+
+}
